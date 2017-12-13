cce55c73cd3b1527113980c461ebcbebb780cbbd
Artur Knopik <artur.knopik.1991@gmail.com>
2017-12-13 20:40:20 +0100

    First sfml working project!
    Signed-off-by: Artur Knopik <artur.knopik.1991@gmail.com>

---
+++ cmake-build-debug/CMakeFiles/SlidingPuzzle.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SlidingPuzzle.dir/main.cpp.obj"
+  "../SlidingPuzzle.pdb"
+  "../SlidingPuzzle.exe"
+  "../SlidingPuzzle.exe.manifest"
+  "libSlidingPuzzle.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SlidingPuzzle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/testet.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testet.dir/main.cpp.obj"
+  "testet.pdb"
+  "testet.exe"
+  "testet.exe.manifest"
+  "libtestet.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

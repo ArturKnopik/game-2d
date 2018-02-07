ad8aee39ab93e3e9a3bce71db00fb4651a39365b
Artur Knopik <artur.knopik.1991@gmail.com>
2018-02-08 00:54:45 +0100

    Working with linux, don't know how remove undefined reference on linux, windows work good

---
+++ include/Blocks/Player_test.h
@@ -0,0 +1,14 @@
+//
+// Created by Streaming on 2018-02-04.
+//
+
+#ifndef SFML_GAME_PLAYER_TEST_H
+#define SFML_GAME_PLAYER_TEST_H
+
+
+class Player_test {
+
+};
+
+
+#endif //SFML_GAME_PLAYER_TEST_H
---
+++ src/Blocks/Player_test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Streaming on 2018-02-04.
+//
+
+#include "Player_test.h"
